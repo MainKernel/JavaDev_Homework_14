944c873d0c90a39b5274f47c90df4867c997cfae
mightyloot <orkingua@gmail.com>
2024-03-01 16:33:58 +0200

    Few fixes

--- src/main/java/utils/DataBase.java
+++ src/main/java/utils/DataBase.java
@@ -1,6 +1,6 @@
 package utils;
 
-import jakarta.annotation.PostConstruct;
+
 import models.Note;
 import org.springframework.stereotype.Component;
 
--- src/test/java/services/NoteServiceTest.java
+++ src/test/java/services/NoteServiceTest.java
@@ -3,8 +3,6 @@ package services;
 import models.Note;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import repositories.NoteDao;
 import utils.DataBase;
 
